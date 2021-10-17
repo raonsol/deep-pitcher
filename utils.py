7328656106ded3c8e4db97b9c9ad40a93130c009
raonsol <windows0903@gmail.com>
2021-10-17 17:42:25 +0900

    ADD: function get_filename

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+
+
 def yes_or_no(question):
     """Creates y/n question with handling invalid inputs within console
 
@@ -14,13 +17,40 @@ def yes_or_no(question):
             return False
 
 
-def print_results(total_cnt, failed, skip_cnt, print_failed=True):
-    failed_cnt = len(failed)
-    success_cnt = total_cnt - failed_cnt - skip_cnt
+def print_results(total_cnt, errors, skip_cnt, print_failed=True):
+    error_cnt = len(errors)
+    success_cnt = total_cnt - error_cnt - skip_cnt
     print(
-        f"{success_cnt} items extracted, {skip_cnt} items skipped, {failed_cnt} items failed"
+        f"{success_cnt} items extracted, {skip_cnt} items skipped, {error_cnt} items failed"
     )
-    if print_failed and failed:
+    if print_failed and errors:
         print("Failed items:")
-        for item in failed:
+        for item in errors:
             print(item)
+
+
+def get_paths(in_path):
+    """
+    returns list of the absolute paths of the files in the input dir
+
+    :param in_path: parent path of the files
+    :type in_path: String or PathLike object
+    :return: list of absolute paths of files
+    :rtype: list
+    """
+    # not including subdirectory files
+    file_list = [
+        x for x in os.listdir(in_path) if (x.endswith(".wav") or x.endswith(".WAV"))
+    ]
+    return file_list
+
+
+def get_filename(in_path):
+    """
+    returns only file name without extension
+
+    :param in_path: path to get filename
+    :type in_path: String or PathLike object
+    :rtype: os.path object
+    """
+    return os.path.splitext(os.path.basename(in_path))[0]
