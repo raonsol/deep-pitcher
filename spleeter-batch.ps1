3f28c4a436609b53af345a20a5688c88c0eee60c
raonsol <raonsol@kakao.com>
2021-11-11 17:03:34 +0900

    ADD: to remove stem info suffix

--- spleeter-batch.ps1
+++ spleeter-batch.ps1
@@ -12,5 +12,9 @@ foreach ($f in $batch) {
     spleeter separate -o $out_path -p spleeter:2stems-16kHz -f "{filename}_{instrument}.{codec}" $f.Group
 }
 
-Get-ChildItem $out_path -filter *_accompanimnet.wav | Move-Item -Destination "$out_path/accompanimnet"
 Get-ChildItem $out_path -filter *_vocals.wav | Move-Item -Destination "$out_path/vocals"
+Get-ChildItem $out_path -filter *_accompanimnet.wav | Move-Item -Destination "$out_path/accompanimnet"
+
+# rename to exclude stem info
+Get-ChildItem "$out_path./vocals" | rename-item -newname {$_.name -replace "_vocals", ""}
+Get-ChildItem "$out_path./accompanimnet" | rename-item -newname {$_.name -replace "_accompanimnet", ""}
